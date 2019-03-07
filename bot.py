2edffc4231720172e7f7adad8b649f99339c58fc
stas.frantsuzov <stas.frantsuzov@mail.ru>
2019-03-07 14:19:04 +0300

    refactor bot lib

--- bot.py
+++ bot.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
 import config
-import telebot
-
-bot = telebot.TeleBot(config.token)
+from telegram.ext import Updater, CommandHandler
 
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):  # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+def hello(bot, update):
+    update.message.reply_text('Hello {}'.format(update.message.from_user.first_name))
 
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+updater = Updater(config.token)
+
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+updater.start_polling()
+updater.idle()
+
+# @bot.message_handler(content_types=["text"])
+# def repeat_all_messages(message):  # Название функции не играет никакой роли, в принципе
+#     bot.send_message(message.chat.id, message.text)
+#
+#
+# if __name__ == '__main__':
+#     bot.polling(none_stop=True)
