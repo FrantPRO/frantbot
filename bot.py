4fdd6dc3e32aeda488787f1d2c9e5620a26424e5
stas.frantsuzov <stas.frantsuzov@mail.ru>
2019-03-07 15:01:41 +0300

    refactor all

--- bot.py
+++ bot.py
@@ -1,23 +1,78 @@
 # -*- coding: utf-8 -*-
 import config
-from telegram.ext import Updater, CommandHandler
+import requests
+import datetime
+
+token = config.token
 
 
-def hello(bot, update):
-    update.message.reply_text('Hello {}'.format(update.message.from_user.first_name))
+class BotHandler:
+
+    def __init__(self, token):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+
+    def get_updates(self, offset=None, timeout=30):
+        method = 'getUpdates'
+        params = {'timeout': timeout, 'offset': offset}
+        resp = requests.get(self.api_url + method, params)
+        result_json = resp.json()['result']
+        return result_json
+
+    def send_message(self, chat_id, text):
+        params = {'chat_id': chat_id, 'text': text}
+        method = 'sendMessage'
+        resp = requests.post(self.api_url + method, params)
+        return resp
+
+    def get_last_update(self):
+        get_result = self.get_updates()
+
+        if len(get_result) > 0:
+            last_update = get_result[-1]
+        else:
+            last_update = get_result[len(get_result)]
+
+        return last_update
 
 
-updater = Updater(config.token)
+greet_bot = BotHandler(token)
+greetings = ('здравствуй', 'привет', 'ку', 'здорово')
+now = datetime.datetime.now()
 
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
-updater.start_polling()
-updater.idle()
+def main():
+    new_offset = None
+    today = now.day
+    hour = now.hour
 
-# @bot.message_handler(content_types=["text"])
-# def repeat_all_messages(message):  # Название функции не играет никакой роли, в принципе
-#     bot.send_message(message.chat.id, message.text)
-#
-#
-# if __name__ == '__main__':
-#     bot.polling(none_stop=True)
+    while True:
+        greet_bot.get_updates(new_offset)
+
+        last_update = greet_bot.get_last_update()
+
+        last_update_id = last_update['update_id']
+        last_chat_text = last_update['message']['text']
+        last_chat_id = last_update['message']['chat']['id']
+        last_chat_name = last_update['message']['chat']['first_name']
+
+        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
+            today += 1
+
+        new_offset = last_update_id + 1
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
