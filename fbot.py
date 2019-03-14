01391967bae9051f93081af96d6854607c5a4ddf
stas.frantsuzov <stas.frantsuzov@mail.ru>
2019-03-14 15:10:47 +0300

    update fbot

--- fbot.py
+++ fbot.py
@@ -10,5 +10,6 @@ def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.text)
     print(message.chat.id, message.text)
 
+
 if __name__ == '__main__':
     bot.polling(none_stop=True)
