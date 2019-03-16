8c8f776bd803df97c90f8fc59bbd5afc2d865593
Stas <stas.frantsuzov@mail.ru>
2019-03-16 16:20:06 +0300

    update

---
+++ settings.py
@@ -0,0 +1,6 @@
+import os
+
+TOKEN = os.environ.get("TOKEN")
+HOST = 'https://frantbot.herokuapp.com/'
+PORT = os.environ.get("PORT") # 443, 80, 88 or 8443 (port need to be 'open')
+NAME = os.environ.get("NAME")
