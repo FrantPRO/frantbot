ad178871100de7f7e079684f463d74b08671a78b
Stas <stas.frantsuzov@mail.ru>
2019-03-16 17:10:49 +0300

    update

--- fbot.py
+++ fbot.py
@@ -18,16 +18,8 @@ WEBHOOK_HOST = settings.HOST
 WEBHOOK_PORT = settings.PORT
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
-WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
-
-# Quick'n'dirty SSL certificate generation:
-#
-# openssl genrsa -out webhook_pkey.pem 2048
-# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
-#
-# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
-# with the same value in you put in WEBHOOK_HOST
+WEBHOOK_SSL_CERT = settings.SSL_CERT
+WEBHOOK_SSL_PRIV = settings.SSL_PRIV
 
 WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
 WEBHOOK_URL_PATH = "/{}/".format(API_TOKEN)
--- settings.py
+++ settings.py
@@ -1,6 +1,11 @@
 import os
 
 TOKEN = os.environ.get("TOKEN")
+
 HOST = 'https://frantbot.herokuapp.com/'
+
 PORT = os.environ.get("PORT") # 443, 80, 88 or 8443 (port need to be 'open')
-NAME = os.environ.get("NAME")
+
+SSL_CERT = 'https://frantbot.herokussl.com/' # './webhook_cert.pem'  # Path to the ssl certificate
+
+SSL_PRIV = 'https://frantbot.herokussl.com/' # './webhook_pkey.pem'  # Path to the ssl private key
