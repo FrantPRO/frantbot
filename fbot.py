98264b1b36134f8cfe4d5d9593e1c0b7ba877ddd
Stanislav Frantsuzov <stas.frantsuzov@mail.ru>
2019-03-29 19:01:45 +0300

    Update fbot.py
    Update

--- fbot.py
+++ fbot.py
@@ -5,14 +5,16 @@ import telebot
 bot = telebot.TeleBot(settings.TOKEN)
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcom(message):
     bot.reply_to(message, "Hello MF!")
 
+@bot.message_handler(commands=['help'])
+def send_welcom(message):
+    bot.send_mesaage(message.chat.id, 'Help your self')
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-    #bot.reply_to(message, message.text)
     bot.send_message(message.chat.id, message.text)
 
 
