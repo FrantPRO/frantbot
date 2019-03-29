17f0c0ba31e6ee0a2a526b9d2b37a587ea6156ad
Stas <stas.frantsuzov@mail.ru>
2019-03-29 16:28:42 +0300

    update fbot

--- fbot.py
+++ fbot.py
@@ -5,14 +5,15 @@ import telebot
 bot = telebot.TeleBot(settings.TOKEN)
 
 
-@bot.message_handler(commands=['start', [help]])
+@bot.message_handler(commands=['start', ['help']])
 def send_welcom(message):
     bot.reply_to(message, "Hello MF!")
 
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
+    #bot.reply_to(message, message.text)
+    bot.send_message(message.chat.id, message.text)
 
 
 bot.polling()
