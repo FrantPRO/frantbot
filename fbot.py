155125acc721a757ee380661393cb9b6e8f7bc77
Stas <stas.frantsuzov@mail.ru>
2019-03-29 16:14:19 +0300

    update fbot.py

--- fbot.py
+++ fbot.py
@@ -1,46 +1,18 @@
 import settings
-import asyncio
-import aiohttp
-from aiohttp import web
-import json
-
-TOKEN = settings.TOKEN
-API_URL = 'https://api.telegram.org/bot%s/sendMessage' % TOKEN
+import telebot
 
 
-async def handler(request):
-    data = await request.json()
-    headers = {
-        'Content-Type': 'application/json'
-    }
-    message = {
-        'chat_id': data['message']['chat']['id'],
-        'text': data['message']['text']
-    }
-    async with aiohttp.ClientSession(loop=loop) as session:
-        async with session.post(API_URL,
-                                data=json.dumps(message),
-                                headers=headers) as resp:
-            try:
-                assert resp.status == 200
-            except:
-                return web.Response(status=500)
-    return web.Response(status=200)
+bot = telebot.TeleBot(settings.TOKEN)
 
 
-async def init_app(loop):
-    app = web.Application(loop=loop, middlewares=[])
-    app.router.add_post('/api/v1', handler)
-    return app
+@bot.message_handler(commands=['start', [help]])
+def send_welcom(message):
+    bot.reply_to(message, "Hello MF!")
 
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    try:
-        app = loop.run_until_complete(init_app(loop))
-        web.run_app(app, host='0.0.0.0', port=23456)
-    except Exception as e:
-        print('Error create server: %r' % e)
-    finally:
-        pass
-    loop.close()
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+bot.polling()
