800a4f3071b32c7b7a985588261913ace27c9af9
Stanislav Frantsuzov <stas.frantsuzov@mail.ru>
2019-03-30 12:51:03 +0300

    Update fbot.py

--- fbot.py
+++ fbot.py
@@ -15,7 +15,7 @@ def send_help(message):
 def send_kurs(message):
     kurs=0
     text = message.text
-    if len(text) = 9:
+    if len(text) == 9:
         currency = text[6,9]
     else:
         currency = 'non'
