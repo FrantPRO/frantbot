e077843d29d9c7961583c2ce94402db56ee8281f
Stas <stas.frantsuzov@mail.ru>
2019-04-03 12:48:40 +0300

    update

--- fbot.py
+++ fbot.py
@@ -4,13 +4,19 @@ import exchange_rates
 
 bot = telebot.TeleBot(settings.TOKEN)
 
+
 @bot.message_handler(commands=['start'])
 def send_welcom(message):
     bot.reply_to(message, "Hello friend!")
 
+
 @bot.message_handler(commands=['help'])
 def send_help(message):
-    bot.send_message(message.chat.id, 'Help your self')
+    text = '/start - Start the bot\n' \
+           '/help - about menu\n' \
+           '/kurs - Kurs valut (usd. eur)'
+    bot.send_message(message.chat.id, text)
+
 
 @bot.message_handler(commands=['kurs'])
 def send_kurs(message):
@@ -21,9 +27,11 @@ def send_kurs(message):
         kurs = exchange_rates.get_kurs_eur_today()
     bot.send_message(message.chat.id, kurs)
 
+
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
     bot.send_message(message.chat.id, message.text)
+
 
 if __name__ == '__main__':
     bot.polling()
