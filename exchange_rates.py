719d8868e650af9142f3e4f6ef11c788f3d57178
Stas <stas.frantsuzov@mail.ru>
2019-04-03 12:32:16 +0300

    update

--- exchange_rates.py
+++ exchange_rates.py
@@ -1,11 +1,28 @@
-import urllib.request
-from xml.dom import minidom
+import requests
+import datetime
 
-url = "http://www.cbr.ru/scripts/XML_daily.asp"
+base_url = 'http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1={date_start}&date_req2={date_finish}&VAL_NM_RQ={currency_code}'
+usd = 'R01235'
+eur = 'R01239'
+date_start = datetime.date.today().strftime('%d/%m/%Y')
+date_finish = date_start
 
-webFile = urllib.request.urlopen(url)
 
-if webFile.code == 200:
-    data = webFile.read()
-    md = minidom.parse(data)
-    print(md.getElementsByTagName('USD'))
+def get_kurs_usd_today():
+    url = base_url.format(date_start=date_start, date_finish=date_finish, currency_code=usd)
+    r = requests.get(url)
+    if r.status_code == 200 and r.text:
+        begin = r.text.find('<Value>')
+        finish = r.text.find('</Value>')
+        if begin > 0:
+            return r.text[begin+7:finish]
+
+
+def get_kurs_eur_today():
+    url = base_url.format(date_start=date_start, date_finish=date_finish, currency_code=eur)
+    r = requests.get(url)
+    if r.status_code == 200 and r.text:
+        begin = r.text.find('<Value>')
+        finish = r.text.find('</Value>')
+        if begin > 0:
+            return r.text[begin+7:finish]
--- fbot.py
+++ fbot.py
@@ -16,14 +16,14 @@ def send_help(message):
 def send_kurs(message):
     kurs = 0
     if 'usd' in message.text:
-        kurs = 1
+        kurs = exchange_rates.get_kurs_usd_today()
     elif 'eur' in message.text:
-        kurs = 2
+        kurs = exchange_rates.get_kurs_eur_today()
     bot.send_message(message.chat.id, kurs)
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
     bot.send_message(message.chat.id, message.text)
 
-if __name__ == 'main':
+if __name__ == '__main__':
     bot.polling()
