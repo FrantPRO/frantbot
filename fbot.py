caa0b2d4ea5799b97c036f08a007b523a254e4a0
Stas <stas.frantsuzov@mail.ru>
2019-04-10 17:59:44 +0300

    fix

--- fbot.py
+++ fbot.py
@@ -65,4 +65,4 @@ def webhook():
 
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
