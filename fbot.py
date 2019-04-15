e30a18d5537e68de9164fff39448adb68b936a6d
Stas <stas.frantsuzov@mail.ru>
2019-04-15 14:11:55 +0300

    fix

--- fbot.py
+++ fbot.py
@@ -34,7 +34,7 @@ class BotComm:
         self.dp = Dispatcher(self.bot, self.update_queue)
 
         self.dp.add_handler(CommandHandler("start", self._start))
-        # self.dp.add_handler(MessageHandler(Filters.text, self._process_update))
+        self.dp.add_handler(MessageHandler(Filters.text, self._process_update))
         self.dp.add_error_handler(self._error)
 
     @cherrypy.tools.json_in()
@@ -52,55 +52,8 @@ class BotComm:
         print("!!!!!! start")
         update.effective_message.reply_text('Hello friend!')
 
-    # @bot.message_handler(commands=['help'])
-    # def send_help(message):
-    #     text = 'My first bot - echobot\n' \
-    #            '/start - Start the bot\n' \
-    #            '/help - about menu\n' \
-    #            '/kurs usd - Kurs valut (usd. eur)'
-    #     bot.send_message(message.chat.id, text)
-
-    # @bot.message_handler(func=lambda m: True)
-    # def echo_all(message):
-    #     bot.send_message(message.chat.id, message.text)
-
-    # @bot.message_handler(commands=['kurs'])
-    # def send_kurs(message):
-    #     currency_info = None
-    #     if 'usd' in message.text:
-    #         currency_info = exchange_rates.get_rate_usd()
-    #
-    #     elif 'eur' in message.text:
-    #         currency_info = exchange_rates.get_rate_eur()
-    #
-    #     if currency_info:
-    #         text = 'Курс ' + currency_info.get('currency') + ': ' + currency_info.get(
-    #             'value') + ' (' + currency_info.get(
-    #             'date') + ')'
-    #     else:
-    #         text = 'currency not found'
-    #
-    #     bot.send_message(message.chat.id, text)
-
-    # def _accept_order(self, bot, update):
-    #     chat_id = update.effective_message.chat.id
-    #     order_text = update.effective_message.text
-    #     order_user = update.effective_message.from_user
-    #     order_user_first_name = order_user.first_name
-    #     order_user_last_name = order_user.last_name
-    #     order_user_username = order_user.username
-    #     text = "{first_name} {last_name} ({username}) " \
-    #            "желает: {order}".format(first_name=order_user_first_name,
-    #                                     last_name=order_user_last_name,
-    #                                     username=order_user_username,
-    #                                     order=order_text)
-    #     self.bot.send_message(chat_id=chat_id, text=text)
-    #     update.effective_message.reply_text("Ваш заказ принят!")
-
-    # def _process_update(self, bot, update):
-    #     chat_id = update.effective_message.chat.id
-    #     if chat_id == 112789249:
-    #         self.bot.send_message(chat_id=chat_id, text="")
+    def _process_update(self, bot, update):
+        self.bot.send_message(chat_id=update.effective_message.chat.id, text="")
 
 
 if __name__ == "__main__":
