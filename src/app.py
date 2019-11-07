6d864e5d16b8118e3b94d97250db6047ccbc54fa
Станислав Французов <s.frantsuzov@realweb.ru>
2019-11-07 12:10:18 +0300

    fix

--- src/app.py
+++ src/app.py
@@ -58,7 +58,7 @@ class BotComm:
         update.effective_message.reply_text("Hello " + update.effective_message.from_user.first_name + "!")
 
     def _help(self, bot, update):
-        text = "My first bot\n" \
+        text = "The Bot\n" \
                + "/start - start the bot\n" \
                + "/help - about menu\n" \
                + "/k - kurs valut (usd, eur, etc)\n" \
