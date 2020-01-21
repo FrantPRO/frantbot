615edc2328c639f4905b36cc42f262264d45b703
Станислав Французов <s.frantsuzov@realweb.ru>
2020-01-21 12:20:36 +0300

    corrected method weather forecast for several cities, fixed

--- src/service.py
+++ src/service.py
@@ -123,10 +123,8 @@ def weather_forecast(city, weather_key, timezone_key):
                                                  lon=city_data["coord"]["lon"]))
             cur_time = resp_cur_time.json()
 
-            if res:
-                res += "\n"
-
-            res += "{city} {country}\n" \
+            res += "* {city} {country}\n" \
+                   "geo: [{coord}]\n" \
                    "{date}\n" \
                    "Temp: {temp} *C\n" \
                    "Wind: {wind} m/s {wind_dir}\n" \
@@ -136,6 +134,7 @@ def weather_forecast(city, weather_key, timezone_key):
                    "Description: {desc}" \
                 .format(city=city_data["name"],
                         country=city_data["sys"]["country"],
+                        coord=str(city_data["coord"]["lon"]) + ", " + str(city_data["coord"]["lat"]),
                         date=datetime.datetime.fromtimestamp(cur_time.get("timestamp", 0)).strftime("%d.%m.%Y %H:%M"),
                         temp=round(city_data["main"]["temp"]),
                         wind=str(city_data["wind"]["speed"]),
